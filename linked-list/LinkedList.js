92d7233c0b486133198e24ad6a2e91ee08b9ecf9
Admin <admin@admins-mbp.myfiosgateway.com>
2025-06-25 10:12:42 -0400

    added reverse method to linkedlist file

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -146,8 +146,24 @@ class LinkedList {
         before.next = temp.next
         temp.next = null
         this.length--
-        return temp
-        
+        return temp  
+    }
+
+    reverse() {
+        let temp = this.head
+        this.head = this.tail
+        this.tail = temp
+
+        let next = temp.next
+        let prev = null
+
+        for(i = 0; i < this.length; i++ ) {
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+        }
+        return this
     }
 }
 
